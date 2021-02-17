d627eb4ecd6a95c3f777ff888a25068ed6622d57
Sophia Hwang <sophih6@uw.edu>
2021-02-17 12:05:13 -0800

    markdown

--- analysis.R
+++ analysis.R
@@ -24,14 +24,19 @@ country_data <- data %>%
 
 # High level questions ---------------------------
 
-# From how many countries do asylum seekers come from 
-num_countries <- country_data %>% 
+# From how many countries do asylum seekers come from (into county of interest)?
+num_countries <- nrow(country_data)
+
+# How many people sought asylum in the country in 2020?
+num_people <- country_data %>% 
   summarize(total_people = sum(Asylum.seekers)) %>% 
   pull()
 
 top_10_countries <- country_data %>% 
   top_n(10, wt = Asylum.seekers) %>% 
+  arrange(-Asylum.seekers) %>% 
   select(Country.of.origin, Asylum.seekers)
+
 
 # Make a map -----------------
 shapefile <- map_data("world")
@@ -39,7 +44,7 @@ shapefile <- shapefile %>%
   mutate(Country.of.origin..ISO. = countrycode(region, origin = 'country.name', destination = 'iso3c')) %>% 
   left_join(country_data, by = "Country.of.origin..ISO.")
 
-ggplot(data = shapefile) + 
+asylum_map <- ggplot(data = shapefile) + 
   geom_polygon(mapping = aes(x = long, y = lat, group = group, fill = Asylum.seekers)) + 
   labs(title = paste("Number of People Seeking Asylum in", country_name),
        x = "", y = "", fill = "Num. People") +
